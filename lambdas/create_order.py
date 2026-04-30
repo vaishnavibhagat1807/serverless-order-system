a7e2f263835db8e3772d77207b396b469e133d30
VSB <vaishnavibhagat1807@gmail.com>
2026-04-30 17:57:00 +0530

    Update create_order.py

--- lambdas/create_order.py
+++ lambdas/create_order.py
@@ -1,13 +1,32 @@
 import json
+import boto3
 import uuid
+
+sqs = boto3.client('sqs')
+
+QUEUE_URL = "https://sqs.ap-south-1.amazonaws.com/586337033262/order-queue"
 
 def lambda_handler(event, context):
     order_id = str(uuid.uuid4())
-
+    
+    body = json.loads(event['body'])
+    
+    order = {
+        "orderId": order_id,
+        "item": body.get("item"),
+        "status": "CREATED"
+    }
+    
+    sqs.send_message(
+        QueueUrl=QUEUE_URL,
+        MessageBody=json.dumps(order)
+    )
+    
     return {
         'statusCode': 200,
         'body': json.dumps({
-            'message': 'Order Created',
-            'order_id': order_id
+            'message': 'Order placed',
+            'orderId': order_id
         })
     }
+
