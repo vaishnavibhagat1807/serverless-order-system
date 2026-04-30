4e4c1207a4ccbfc111b437f6efdae887afe4b073
VSB <vaishnavibhagat1807@gmail.com>
2026-04-30 17:57:56 +0530

    Update process_order.py

--- lambdas/process_order.py
+++ lambdas/process_order.py
@@ -1,3 +1,35 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Orders')
+
+sns = boto3.client('sns')
+TOPIC_ARN = "arn:aws:sns:ap-south-1:586337033262:order-notification"
+
 def lambda_handler(event, context):
-    print("Processing order:", event)
-    return {"status": "processed"}
+    print("Full event:", event)
+
+    for record in event['Records']:
+        print("Processing record:", record)
+
+        order = json.loads(record['body'])
+        print("Order data:", order)
+
+        item = {
+            "orderId": str(order["orderId"]),
+            "item": str(order.get("item", "")),
+            "status": str(order.get("status", "CREATED"))
+        }
+
+        print("Final item to insert:", item)
+
+        table.put_item(Item=item)
+
+        sns.publish(
+            TopicArn=TOPIC_ARN,
+            Message=f"Order {item['orderId']} processed"
+        )
+
+
+      
